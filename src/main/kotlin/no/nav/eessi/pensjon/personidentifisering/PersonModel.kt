c8bf923d9a1797b704a41787e1ac419ae5244b47
Daniel Skarpås <dskarpas@gmail.com>
2021-11-22 10:17:06 +0100

    ep-1246 mc sletting av metode som ikke er i bruk/flyttet
    Co-authored-by: Jens Christian Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/personidentifisering/PersonModel.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/personidentifisering/PersonModel.kt
@@ -6,27 +6,7 @@ import no.nav.eessi.pensjon.personoppslag.pdl.model.UtenlandskIdentifikasjonsnum
 data class IdentifisertPerson(
     val personIdenterFraSed: PersonIdenter,
     val uidFraPdl: List<UtenlandskIdentifikasjonsnummer> = emptyList()
-) {
-//    fun validateSedUidAgainstPdlUid(fl: FinnLand) : IdentifisertPerson? {
-//        //pdl pair (land, ident)
-//        val pdlPair = uidFraPdl.map { Pair(it.utstederland, it.identifikasjonsnummer) }
-//
-//        //make new seduid validatet against pdluid (contrycode, ident) map use interface FinnLand (iso2->iso3) SE->SWE
-//        val newSedUid = personIdenterFraSed.uid
-//            .mapNotNull { seduid -> fl.finnLandkode(seduid.utstederland)?.let {  UtenlandskPin(seduid.kilde, seduid.identifikasjonsnummer, it) } }
-//            .filterNot { seduid ->
-//                //sed pair (land, ident)
-//                val seduidPair = Pair( seduid.utstederland , seduid.identifikasjonsnummer)
-//                //filter current seduidPair in all pdlPair
-//                seduidPair in pdlPair
-//            }
-//        if (newSedUid.isEmpty()) return null //no new uid to add to pdl
-//
-//        val newpersonIdenterFraSed = this.personIdenterFraSed.copy(uid = newSedUid)
-//        return this.copy(personIdenterFraSed = newpersonIdenterFraSed, uidFraPdl = emptyList()) //new ident with uid not in pdl
-//    }
-
-}
+)
 
 data class PersonIdenter(
     val fnr: Fodselsnummer?,
