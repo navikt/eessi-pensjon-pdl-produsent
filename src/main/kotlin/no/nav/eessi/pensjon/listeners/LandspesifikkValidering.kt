562b5a3aae43b735273138db43181baab8be69ad
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2021-11-23 13:49:45 +0100

    EP-1246 microcimmit stuff oppå anet stuff
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Christian Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValidering.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValidering.kt
@@ -10,14 +10,17 @@ class LandspesifikkValidering() {
     fun validerLandsspesifikkUID(landkode: String, uid: String): Boolean {
         logger.debug("valider land: $landkode, uid: $uid")
         return when (landkode) {
-            "BEL" -> uid.length == 13 && uid.replace("-","") .length == 11 && uid.substring(6, 7) == "-" && uid.substring(10, 11) == "-"
-            "BGR" -> uid.length != 10
-            "FIN" -> uid.length != 11 || uid.substring(6, 7) != "-" || uid.substring(8, 9) != "-"
+            "BEL" -> uid.length == 13 && uid.checkDigitsLength(11) && uid.substring(6, 7) == "-" && uid.substring(10, 11) == "-"
+            "BGR" -> uid.checkDigitsLength(10)
+            "FIN" -> uid.length == 11 && uid.substring(6,7) in listOf("-","A","a") && uid.checkDigitsLength(IntRange(0,5), 6) && uid.checkDigitsLength(IntRange(7, 9), 3)
             "ISL" -> uid.length != 11 || uid.substring(6, 7) != "-" || uid.substring(8, 9) != "-"
-            "DNK" -> uid.length != 11 || uid.substring(6, 7) != "-" || uid.substring(8, 9) != "-"
+            "DNK" -> uid.replace("[-:./\\s]".toRegex(), "").also { logger.debug(it) } .length == 11
             else ->  false
         }.also { logger.debug("$landkode -> result  of our dreams: $it") }
     }
 
+    fun String.checkDigitsLength(range: IntRange, len: Int): Boolean =  this.substring(range).checkDigitsLength(len)
+
+    fun String.checkDigitsLength(len: Int): Boolean = this.filter { it.isDigit() }.length == len
 
 }
--- src/test/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValideringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValideringTest.kt
@@ -12,8 +12,14 @@ internal class LandspesifikkValideringTest {
     @CsvSource(
         "BEL, 770113-123-12, true",
         "BEL, 770113-12312, false",
-        "BGR, 12345678901, true" ,
-        "FIN, 770113-123-12, true",
+        "BGR, 1234567890, true" ,
+        "BGR, 123456789012, false" ,
+        "BGR, 770113-12312, false",
+        "FIN, 130177A636D, true",
+        "FIN, 130177-308T, true",
+        "FIN, 130177-5800, true",
+        "FIN, 130177A5800, true",
+        "FIN, 130177A636D, true",
         "ISL, 770113-123-12, true",
         "DNK, 770113-123-12, true")
     fun `Gitt en  Belgisk UID som skal valideres mot landspesifikk formatering saa skal den returnere gyldig`(land : String, uid : String, check: Boolean) {
