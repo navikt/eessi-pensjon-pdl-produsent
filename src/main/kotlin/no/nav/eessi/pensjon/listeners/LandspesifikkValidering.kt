47bbb382ec83d475add8841b588f1c5be7119889
mariampervez <mariam.pervez@nav.no>
2021-11-23 15:29:35 +0100

    Legger til regler for LandspesifikkValidering samt tester
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValidering.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValidering.kt
@@ -13,14 +13,22 @@ class LandspesifikkValidering() {
             "BEL" -> uid.length == 13 && uid.checkDigitsLength(11) && uid.substring(6, 7) == "-" && uid.substring(10, 11) == "-"
             "BGR" -> uid.checkDigitsLength(10)
             "FIN" -> uid.length == 11 && uid.substring(6,7) in listOf("-","A","a") && uid.checkDigitsLength(IntRange(0,5), 6) && uid.checkDigitsLength(IntRange(7, 9), 3)
-            "ISL" -> uid.length != 11 || uid.substring(6, 7) != "-" || uid.substring(8, 9) != "-"
-            "DNK" -> uid.replace("[-:./\\s]".toRegex(), "").also { logger.debug(it) } .length == 11
+            "ISL", "DNK" -> uid.length == 11 && uid.checkDigitsLength(10) && uid.substring(5, 6) != "-" && uid.checkDigitsLength(IntRange(0,5), 6) && uid.checkDigitsLength(IntRange(7,10), 4)
+            "EST", "LTU", "POL" -> uid.checkDigitsLength(11) && uid.length == 11
+            "ITA" -> uid.isLettersOrDigit(16)
+            "LVA" -> uid.checkDigitsLength(11) && uid.substring(5, 6) != "-" && uid.checkDigitsLength(IntRange(0,6), 6) && uid.checkDigitsLength(IntRange(6,11), 5)
+            "NLD" -> uid.length == 11 && uid.checkDigitsLength(9) && uid.substring(4, 5) == "." && uid.substring(7, 8) == "."
+            "SLO" -> uid.checkDigitsLength(13) || uid.checkDigitsLength(8)
             else ->  false
         }.also { logger.debug("$landkode -> result  of our dreams: $it") }
     }
 
     fun String.checkDigitsLength(range: IntRange, len: Int): Boolean =  this.substring(range).checkDigitsLength(len)
 
-    fun String.checkDigitsLength(len: Int): Boolean = this.filter { it.isDigit() }.length == len
+    fun String.checkDigitsLength(length: Int): Boolean = this.filter { it.isDigit() }.length == length
+
+    fun String.isLettersOrDigit(length: Int): Boolean {
+        return this.none { it !in 'A'..'Z' && it !in 'a'..'z' && it !in '0'..'9' } && this.length == length
+    }
 
 }
--- src/test/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValideringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValideringTest.kt
@@ -20,8 +20,34 @@ internal class LandspesifikkValideringTest {
         "FIN, 130177-5800, true",
         "FIN, 130177A5800, true",
         "FIN, 130177A636D, true",
-        "ISL, 770113-123-12, true",
-        "DNK, 770113-123-12, true")
+        "ISL, 130177-7159, true",
+        "ISL, 130177-71591, false",
+        "ISL, 1301777159, false",
+        "ISL, 1301777159-, false",
+        "DNK, 130177-1234, true",
+        "DNK, 130177-12341, false",
+        "EST, 37701132722, true",
+        "EST, 377A1132722, false",
+        "LTU, 37701132722, true",
+        "LTU, 377011-2722X, false",
+        "ITA, PSNSVR77A13B123C, true",
+        "ITA, a2c4e677A13B123C, true",
+        "ITA, PSNSVR77A13B123C-, false",
+        "ITA, PSNSVRA13B123C-, false",
+        "ITA, PSNSVR%123C-, false",
+        "LVA, 130177-18017, true",
+        "LVA, 130177-180171, false",
+        "LVA, 130177-1801Ø, false",
+        "NLD, 1234.56.789, true",
+        "NLD, 130177-1801Ø, false",
+        "NLD, 130177&1801Ø, false",
+        "NLD, 130-1801Ø, false",
+        "POL, 77011312345, true",
+        "POL, 770113123455, false",
+        "POL, 77011312345%, false",
+        "POL, 771---12345%, false",
+    )
+
     fun `Gitt en  Belgisk UID som skal valideres mot landspesifikk formatering saa skal den returnere gyldig`(land : String, uid : String, check: Boolean) {
         assertEquals(check, valdidering.validerLandsspesifikkUID(land, uid))
     }
