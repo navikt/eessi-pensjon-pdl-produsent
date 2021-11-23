aadf1138076ffcd59de78a9b4c10c959ea7b81b1
Daniel Skarpås <dskarpas@gmail.com>
2021-11-23 10:36:21 +0100

    ep-1246 tester for landsspesifikk validering
    Co-authored-by: Jens Christian Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/test/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValideringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValideringTest.kt
@@ -2,15 +2,20 @@ package no.nav.eessi.pensjon.listeners
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class LandspesifikkValideringTest {
 
     private val valdidering = LandspesifikkValidering()
 
     @ParameterizedTest
-    @ValueSource(strings = ["BEL", "BGR", "FIN", "ISL", "DNK"])
-    fun `Gitt en  Belgisk UID som skal valideres mot landspesifikk formatering saa skal den returnere gyldig`(land : String) {
-        assertEquals(true, valdidering.validerLandsspesifikkUID(land, "770113-123-12"))
+    @CsvSource(
+        "BEL, 770113-123-12",
+        "BGR, 770113-123-12",
+        "FIN, 770113-123-12",
+        "ISL, 770113-123-12",
+        "DNK, 770113-123-12")
+    fun `Gitt en  Belgisk UID som skal valideres mot landspesifikk formatering saa skal den returnere gyldig`(land : String, uid : String) {
+        assertEquals(true, valdidering.validerLandsspesifikkUID(land, uid))
     }
 }
