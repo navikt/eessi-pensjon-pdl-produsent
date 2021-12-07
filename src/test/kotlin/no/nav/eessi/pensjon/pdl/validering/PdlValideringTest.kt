1d00493152d2405e56dc8f656fa8c7c203bf8ee6
Daniel Skarpås <dskarpas@gmail.com>
2021-12-07 14:03:05 +0100

    ep-1248 -t validering av gb LandspesifikkValidering
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/validering/PdlValideringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/validering/PdlValideringTest.kt
@@ -6,8 +6,22 @@ import no.nav.eessi.pensjon.personidentifisering.UtenlandskPin
 import no.nav.eessi.pensjon.personoppslag.Fodselsnummer
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
 
 internal class PdlValideringTest {
+
+    @Test
+    fun `Gitt en ny uid naar avsenderland og uidland ikke er det samme saa returner true`() {
+        val pdlValidering = PdlValidering()
+        val identifisertPerson = IdentifisertPerson(
+            PersonIdenter(
+                Fodselsnummer.fra("11067122781"), listOf(
+                    UtenlandskPin("P2000", "1234567891236540", "DE"),
+                )
+            ), emptyList()
+        )
+        assertTrue(pdlValidering.erUidLandAnnetEnnAvsenderLand(listOf(identifisertPerson), "SE"))
+    }
 
     @Test
     fun `Gitt en ny uid naar avsenderland og uidland er det samme saa returner false`() {
@@ -19,6 +33,6 @@ internal class PdlValideringTest {
                 )
             ), emptyList()
         )
-        assertTrue(pdlValidering.erUidLandAnnetEnnAvsenderLand(listOf(identifisertPerson), "SE"))
+        assertFalse (pdlValidering.erUidLandAnnetEnnAvsenderLand(listOf(identifisertPerson), "DE"))
     }
 }
