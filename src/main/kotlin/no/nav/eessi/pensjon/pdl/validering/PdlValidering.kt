ed4ce5265a613a0c16717665ee4df4aebf151463
Mo Amini <mo.amini@nav.no>
2021-12-07 10:05:29 +0100

    Legger til validering av avsender land skal kun få lov til å sende inn nytt UID fra eget land
    Co-authored-by: mariam.pervez@nav.no
    Co-authored-by: daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/validering/PdlValidering.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/validering/PdlValidering.kt
@@ -1,6 +1,5 @@
 package no.nav.eessi.pensjon.pdl.validering
 
-import no.nav.eessi.pensjon.listeners.LandspesifikkValidering
 import no.nav.eessi.pensjon.personidentifisering.IdentifisertPerson
 
 class PdlValidering {
@@ -14,15 +13,32 @@ class PdlValidering {
         return true
     }
 
-    fun validerUid(identifisertPersoner: List<IdentifisertPerson>, avsenderLand: String): List<IdentifisertPerson> {
-        identifisertPersoner.flatMap { it -> it.personIdenterFraSed.uid}
-
+    /**
+     * Kjører landspesifik validering
+     * Aksepterer kun et nytt UID om gangen
+     */
+    fun validerUid(identifisertPersoner: List<IdentifisertPerson>): List<IdentifisertPerson> {
         val validering = LandspesifikkValidering()
+
+        //TODO Lag oppgave når det er flere
         val gyldigepersoner = identifisertPersoner.filter { it.personIdenterFraSed.uid.size == 1 }
+
         return gyldigepersoner.filter { ident ->
             val uid = ident.personIdenterFraSed.uid.first()
             validering.validerLandsspesifikkUID(uid.utstederland, uid.identifikasjonsnummer)
         }
+    }
+
+    fun erUidLandAnnetEnnAvsenderLand(
+        identifisertPersoner: List<IdentifisertPerson>,
+        avsenderLand: String
+    ): Boolean {
+        val alleUidLand =
+            identifisertPersoner.flatMap { it -> it.personIdenterFraSed.uid }.map { it.utstederland }.distinct()
+        if (alleUidLand.any { it != avsenderLand }) {
+            return true
+        }
+        return false
     }
 
     fun finnesAvsenderInstitusjon(avsenderNavn: String?) = avsenderNavn != null
--- src/test/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValideringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/listeners/LandspesifikkValideringTest.kt
@@ -1,5 +1,6 @@
 package no.nav.eessi.pensjon.listeners
 
+import no.nav.eessi.pensjon.pdl.validering.LandspesifikkValidering
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
