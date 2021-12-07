9d6d8f7a84033d5d424a6f823f31fdda20b8962c
Mo Amini <mo.amini@nav.no>
2021-12-07 13:57:58 +0100

    Legger til ny test for å sjekke om avsenderland og Uid land er samme

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/validering/PdlValideringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/validering/PdlValideringTest.kt
@@ -1,19 +1,24 @@
 package no.nav.eessi.pensjon.pdl.validering
 
 import no.nav.eessi.pensjon.personidentifisering.IdentifisertPerson
-import org.junit.jupiter.api.Assertions.*
+import no.nav.eessi.pensjon.personidentifisering.PersonIdenter
+import no.nav.eessi.pensjon.personidentifisering.UtenlandskPin
+import no.nav.eessi.pensjon.personoppslag.Fodselsnummer
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class PdlValideringTest {
 
     @Test
     fun `Gitt en ny uid naar avsenderland og uidland er det samme saa returner false`() {
-
         val pdlValidering = PdlValidering()
-        val listeOverIdentifisertePersoner = listOf(IdentifisertPerson())
-
-
+        val identifisertPerson = IdentifisertPerson(
+            PersonIdenter(
+                Fodselsnummer.fra("11067122781"), listOf(
+                    UtenlandskPin("P2000", "1234567891236540", "DE"),
+                )
+            ), emptyList()
+        )
+        assertTrue(pdlValidering.erUidLandAnnetEnnAvsenderLand(listOf(identifisertPerson), "SE"))
     }
-
-
 }
