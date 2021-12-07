71bd6802cf83acb4170e9c213dd9e89f43bdb72e
mariampervez <mariam.pervez@nav.no>
2021-12-07 13:44:54 +0100

    -t Legge til enhetstest
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/validering/PdlValideringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/validering/PdlValideringTest.kt
@@ -1,12 +1,17 @@
 package no.nav.eessi.pensjon.pdl.validering
 
+import no.nav.eessi.pensjon.personidentifisering.IdentifisertPerson
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class PdlValideringTest {
 
     @Test
-    fun bla() {
+    fun `Gitt en ny uid naar avsenderland og uidland er det samme saa returner false`() {
+
+        val pdlValidering = PdlValidering()
+        val listeOverIdentifisertePersoner = listOf(IdentifisertPerson())
+
 
     }
 
