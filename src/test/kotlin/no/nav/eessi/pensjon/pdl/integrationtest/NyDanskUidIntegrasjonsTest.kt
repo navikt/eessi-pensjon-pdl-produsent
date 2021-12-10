63a1c30952c4ec8e70457a212b613a6cd0c3719c
mariampervez <mariam.pervez@nav.no>
2021-12-10 10:07:14 +0100

    -t Delt opp integrasjonstestene
    Co-authored-by: Mo Amini <mo.amini@nav.no>

---
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/integrationtest/NyDanskUidIntegrasjonsTest.kt
@@ -0,0 +1,99 @@
+package no.nav.eessi.pensjon.pdl.integrationtest
+
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
+import io.mockk.every
+import no.nav.eessi.pensjon.json.mapJsonToAny
+import no.nav.eessi.pensjon.json.toJson
+import no.nav.eessi.pensjon.json.typeRefs
+import no.nav.eessi.pensjon.listeners.SedMottattListener
+import no.nav.eessi.pensjon.personoppslag.pdl.PersonMock
+import no.nav.eessi.pensjon.personoppslag.pdl.PersonService
+import no.nav.eessi.pensjon.personoppslag.pdl.model.NorskIdent
+import no.nav.eessi.pensjon.personoppslag.pdl.model.UtenlandskIdentifikasjonsnummer
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockserver.model.HttpRequest
+import org.mockserver.verify.VerificationTimes
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import java.util.concurrent.TimeUnit
+import kotlin.test.assertTrue
+
+const val PDL_PRODUSENT_TOPIC_MOTATT1 = "eessi-basis-sedmottatt-v1"
+
+@SpringBootTest( classes = [IntegrationBase.TestConfig::class, KafkaTestConfig::class], properties = ["spring.main.allow-bean-definition-overriding=true"])
+@ActiveProfiles("integrationtest")
+@DirtiesContext
+@EmbeddedKafka(
+    topics = [PDL_PRODUSENT_TOPIC_MOTATT1]
+)
+class NyDanskUidIntegrasjonsTest : IntegrationBase() {
+
+    @Autowired(required = true)
+    private lateinit var sedMottattListener: SedMottattListener
+
+    @Autowired
+    private lateinit var template: KafkaTemplate<String, String>
+
+    @Autowired
+    private lateinit var personService: PersonService
+
+    private val deugLogger: Logger = LoggerFactory.getLogger("no.nav.eessi.pensjon") as Logger
+    private val listAppender = ListAppender<ILoggingEvent>()
+
+    @BeforeEach
+    fun setUp() {
+        listAppender.start()
+        deugLogger.addAppender(listAppender)
+    }
+
+
+
+    @Test
+    fun `Gitt en sed hendelse med dansk uid som ikke finnes i pdl skal det opprettes det en endringsmelding til person-mottak`() {
+
+        val fnr = "11067122781"
+        val personMock =  PersonMock.createBrukerWithUid(
+            fnr = fnr,
+            uid = emptyList()
+        )
+
+        every { personService.hentPersonUtenlandskIdent(NorskIdent(fnr)) } returns personMock
+        CustomMockServer()
+            .mockSTSToken()
+            .medSed("/buc/147729/sed/eb938171a4cb4e658b3a6c011962d204", "src/test/resources/eux/sed/P2100-PinDK-NAV.json")
+            .medbBuc("/buc/147729", "src/test/resources/eux/buc/buc279020.json")
+            .medKodeverk("/api/v1/hierarki/LandkoderSammensattISO2/noder", "src/test/resources/kodeverk/landkoderSammensattIso2.json")
+
+        val json = javaClass.getResource("/eux/hendelser/P_BUC_01_P2000-avsenderDK.json")!!.readText()
+        val model = mapJsonToAny(json, typeRefs())
+
+        template.send(PDL_PRODUSENT_TOPIC_MOTATT1, model.toJson()).let {
+            sedMottattListener.getLatch().await(10, TimeUnit.SECONDS)
+
+        assertTrue(validateSedMottattListenerLoggingMessage("Oppretter endringsmelding med nye personopplysninger fra avsenderLand:"))
+
+            mockServer.verify(
+                HttpRequest.request()
+                    .withMethod("POST")
+                    .withPath("/api/v1/endringer"),
+                VerificationTimes.exactly(1)
+            )
+
+        }
+    }
+
+    fun validateSedMottattListenerLoggingMessage(keyword: String): Boolean {
+        val logsList: List<ILoggingEvent> = listAppender.list
+        return logsList.find { logMelding ->
+            logMelding.message.contains(keyword)
+        }?.message?.isNotEmpty() ?: false
+    }
+}
---
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/integrationtest/NySvenskUidSenderTyskInstIntegrasjonsTest.kt
@@ -0,0 +1,95 @@
+package no.nav.eessi.pensjon.pdl.integrationtest
+
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
+import io.mockk.every
+import no.nav.eessi.pensjon.json.mapJsonToAny
+import no.nav.eessi.pensjon.json.toJson
+import no.nav.eessi.pensjon.json.typeRefs
+import no.nav.eessi.pensjon.listeners.SedMottattListener
+import no.nav.eessi.pensjon.personoppslag.pdl.PersonMock
+import no.nav.eessi.pensjon.personoppslag.pdl.PersonService
+import no.nav.eessi.pensjon.personoppslag.pdl.model.NorskIdent
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockserver.model.HttpRequest
+import org.mockserver.verify.VerificationTimes
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import java.util.concurrent.TimeUnit
+import kotlin.test.assertTrue
+
+const val PDL_PRODUSENT_TOPIC_MOTATT2 = "eessi-basis-sedmottatt-v1"
+
+@SpringBootTest( classes = [IntegrationBase.TestConfig::class, KafkaTestConfig::class], properties = ["spring.main.allow-bean-definition-overriding=true"])
+@ActiveProfiles("integrationtest")
+@DirtiesContext
+@EmbeddedKafka(
+    topics = [PDL_PRODUSENT_TOPIC_MOTATT2]
+)
+class NySvenskUidSenderTyskInstIntegrasjonsTest : IntegrationBase() {
+
+    @Autowired(required = true)
+    private lateinit var sedMottattListener: SedMottattListener
+
+    @Autowired
+    private lateinit var template: KafkaTemplate<String, String>
+
+    @Autowired
+    private lateinit var personService: PersonService
+
+    private val deugLogger: Logger = LoggerFactory.getLogger("no.nav.eessi.pensjon") as Logger
+    private val listAppender = ListAppender<ILoggingEvent>()
+
+    @BeforeEach
+    fun setUp() {
+        listAppender.start()
+        deugLogger.addAppender(listAppender)
+    }
+
+    @Test
+    fun `Gitt en sed-hendelse fra Sverige som sender inn en tysk uid så skal det stoppes av valideringen`() {
+
+        val fnr = "29087021082"
+        val personMock =  PersonMock.createBrukerWithUid(
+            fnr = fnr,
+            uid = emptyList()
+        )
+
+        every { personService.hentPersonUtenlandskIdent(NorskIdent(fnr)) } returns personMock
+        CustomMockServer()
+            .mockSTSToken()
+            .medSed("/buc/147729/sed/eb938171a4cb4e658b3a6c011962d204", "src/test/resources/eux/sed/P8000-TyskPIN.json")
+            .medbBuc("/buc/147729", "src/test/resources/eux/buc/buc279020.json")
+            .medKodeverk("/api/v1/hierarki/LandkoderSammensattISO2/noder", "src/test/resources/kodeverk/landkoderSammensattIso2.json")
+
+        val json = javaClass.getResource("/eux/hendelser/P_BUC_01_P2000-avsenderSE.json")!!.readText()
+        val model = mapJsonToAny(json, typeRefs())
+
+        template.send(PDL_PRODUSENT_TOPIC_MOTATT2, model.toJson()).let {
+            sedMottattListener.getLatch().await(10, TimeUnit.SECONDS)
+        }
+
+        assertTrue(validateSedMottattListenerLoggingMessage("Avsenderland mangler eller avsenderland er ikke det samme som uidland, stopper identifisering av personer"))
+
+        mockServer.verify(
+            HttpRequest.request()
+                .withMethod("POST")
+                .withPath("/api/v1/endringer"),
+            VerificationTimes.exactly(0)
+        )
+    }
+
+    fun validateSedMottattListenerLoggingMessage(keyword: String): Boolean {
+        val logsList: List<ILoggingEvent> = listAppender.list
+        return logsList.find { logMelding ->
+            logMelding.message.contains(keyword)
+        }?.message?.isNotEmpty() ?: false
+    }
+}
