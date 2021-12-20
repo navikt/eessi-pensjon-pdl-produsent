66a3004aabc7634083decdb58b760cef6056047f
mariampervez <mariam.pervez@nav.no>
2021-12-20 10:09:43 +0100

    Bumper ep-eux
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/test/kotlin/no/nav/eessi/pensjon/models/SedHendelseTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/models/SedHendelseTest.kt
@@ -1,8 +1,7 @@
 package no.nav.eessi.pensjon.models
 
-import no.nav.eessi.pensjon.eux.model.SedType
 import no.nav.eessi.pensjon.eux.model.buc.BucType
-
+import no.nav.eessi.pensjon.eux.model.SedType
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
