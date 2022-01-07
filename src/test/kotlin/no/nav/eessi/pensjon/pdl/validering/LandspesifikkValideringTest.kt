266fd7ea29ef4110f51cdad9016a4dcff9f9a2b8
mariampervez <mariam.pervez@nav.no>
2022-01-07 10:29:27 +0100

    Legger til doc
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/validering/LandspesifikkValideringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/validering/LandspesifikkValideringTest.kt
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
+
+/**
+ * Lenke til landsspesifikk valideringsregler:
+ * https://confluence.adeo.no/display/MOFO/OMR-321+Utenlandsk+ID+-+6+-+Produsent
+ */
 internal class LandspesifikkValideringTest {
 
     private val valdidering = LandspesifikkValidering()
@@ -69,18 +74,20 @@ internal class LandspesifikkValideringTest {
         "GBR, ZX, false" //length
 
     )
-/*
-    Tyskland: Eks. 56 120157 F 016, 02 140477 T 039.
-    12 tegn, bokstaver og tall, mellomrom etter 2. og 8. tall, samt etter bokstav (bokstav er første i etternavn)
+/**
+ * Regler for andre land vi kommuniserer med
 
-    Storbritannia: Eks. ZX 91 67 77 C9 tegn, bokstaver og tall, mellomrom etter 2. bokstav, samt etter hvert 2. tall.
+ * Tyskland: Eks. 56 120157 F 016, 02 140477 T 039.
+ * 12 tegn, bokstaver og tall, mellomrom etter 2. og 8. tall, samt etter bokstav (bokstav er første i etternavn)
 
-    Ungarn: Eks. 069-441-934.
-    9 tall, bindestrek mellom hver 3. tall.
-    Frankrike: Eks. 2 52 01 75 068 079
-    13 tall, mellomrom etter 1., 3., 5.,7., 10.
-    Spania: Eks. 0X5807635C
-    10 tegn, kun bokstaver og tall
+ * Storbritannia: Eks. ZX 91 67 77 C9 tegn, bokstaver og tall, mellomrom etter 2. bokstav, samt etter hvert 2. tall.
+
+ * Ungarn: Eks. 069-441-934.
+ * 9 tall, bindestrek mellom hver 3. tall.
+ * Frankrike: Eks. 2 52 01 75 068 079
+ * 13 tall, mellomrom etter 1., 3., 5.,7., 10.
+ * Spania: Eks. 0X5807635C
+ * 10 tegn, kun bokstaver og tall
 */
 
     fun `Gitt en UID som skal valideres mot landspesifikk formatering Så skal det retureres boolean`(land : String, uid : String, check: Boolean) {
