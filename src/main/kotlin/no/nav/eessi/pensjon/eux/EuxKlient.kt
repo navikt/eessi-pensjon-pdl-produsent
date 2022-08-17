884d2ad078396be107a5c9aefb458f98c87884f4
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-08-17 13:56:24 +0200

    F fjerner retry mot EUX v/ResourceAccessException

--- src/main/kotlin/no/nav/eessi/pensjon/eux/EuxKlient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/EuxKlient.kt
@@ -4,6 +4,7 @@ import no.nav.eessi.pensjon.eux.model.buc.Buc
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
 import org.springframework.retry.annotation.Backoff
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
@@ -18,23 +19,25 @@ class EuxKlient(private val euxOAuthRestTemplate: RestTemplate) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(EuxKlient::class.java) }
 
     @Retryable(
-        include = [HttpStatusCodeException::class, ResourceAccessException::class],
+        include = [HttpStatusCodeException::class],
         exclude = [HttpClientErrorException.NotFound::class],
         backoff = Backoff(delay = 30000L, maxDelay = 3600000L, multiplier = 3.0)
     )
     internal fun hentSedJson(rinaSakId: String, dokumentId: String): String? {
         logger.info("Henter SED for rinaSakId: $rinaSakId , dokumentId: $dokumentId")
 
-        return euxOAuthRestTemplate.exchange(
+        val exchange: ResponseEntity<String> = euxOAuthRestTemplate.exchange(
             "/buc/$rinaSakId/sed/$dokumentId",
-                HttpMethod.GET,
-                null,
-                String::class.java
-            ).body
+            HttpMethod.GET,
+            null,
+            String::class.java
+        )
+
+    return exchange.body
     }
 
     @Retryable(
-        include = [HttpStatusCodeException::class, ResourceAccessException::class],
+        include = [HttpStatusCodeException::class],
         backoff = Backoff(delay = 30000L, maxDelay = 3600000L, multiplier = 3.0)
     )
     internal fun hentBuc(rinaSakId: String): Buc? {
