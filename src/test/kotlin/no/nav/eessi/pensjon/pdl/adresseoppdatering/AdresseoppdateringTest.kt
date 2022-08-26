8c758137ec7267357de3c211a3245db2ef2ab024
mariampervez <mariam.pervez@nav.no>
2022-08-26 10:48:00 +0200

    t lagt til test for postboksadresse i gatefelt samt ryddet litt
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Kjetil JD <kjetil.jorgensen-dahl@nav.no>

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/AdresseoppdateringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/AdresseoppdateringTest.kt
@@ -37,9 +37,13 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-private const val RINA_SAK_ID = "123456"
-private const val DOKUMENT_ID = "1234"
+private const val SOME_RINA_SAK_ID = "123456"
+private const val SOME_DOKUMENT_ID = "1234"
+private const val SOME_FNR = "1234567799"
+private const val SOME_UTENLANDSK_INSTITUSJON = "Utenlandsk Institusjon"
+
 private const val FNR = "11067122781"
+
 private const val EDDY_ADRESSE_LANDKODE = "SE"
 
 private val EDDY_ADRESSE_I_SED = Adresse(
@@ -71,7 +75,6 @@ private val EDDY_ADRESSE_I_ENDRINGSMELDING = EndringsmeldingUtenlandskAdresse(
     postboksNummerNavn = null // Dersom vi kan identifisere en postboksadresse så burde vi fylle det inn her
 )
 
-
 internal class AdresseoppdateringTest {
 
     var personService: PersonService = mockk()
@@ -98,7 +101,7 @@ internal class AdresseoppdateringTest {
 
     @Test
     fun `Gitt SED med gyldig utlandsadresse, og denne finnes i PDL, saa skal det sendes oppdatering med nye datoer`() {
-        every { euxService.hentSed(eq(RINA_SAK_ID), eq(DOKUMENT_ID)) } returns
+        every { euxService.hentSed(eq(SOME_RINA_SAK_ID), eq(SOME_DOKUMENT_ID)) } returns
                 sed(
                     id = FNR,
                     brukersAdresse = EDDY_ADRESSE_I_SED
@@ -119,10 +122,10 @@ internal class AdresseoppdateringTest {
             sedHendelse(
                 sektor = "P",
                 bucType = "P_BUC_02",
-                rinaSakId = RINA_SAK_ID,
-                avsenderNavn = "Utenlandsk Institusjon",
+                rinaSakId = SOME_RINA_SAK_ID,
+                avsenderNavn = SOME_UTENLANDSK_INSTITUSJON,
                 avsenderLand = EDDY_ADRESSE_LANDKODE,
-                rinaDokumentId = DOKUMENT_ID,
+                rinaDokumentId = SOME_DOKUMENT_ID,
                 sedType = "P2100"
             ))
 
@@ -141,30 +144,25 @@ internal class AdresseoppdateringTest {
 
     @Test
     fun `Gitt SED med adresse med ulik landkode fra avsender, ingen oppdatering`() {
-        every { euxService.hentSed(eq(RINA_SAK_ID), eq(DOKUMENT_ID)) } returns
+        every { euxService.hentSed(eq(SOME_RINA_SAK_ID), eq(SOME_DOKUMENT_ID)) } returns
                 sed(brukersAdresse = adresse(EDDY_ADRESSE_LANDKODE))
 
         val adresseoppdatering = Adresseoppdatering(mockk(), euxService, mockk(), mockk())
 
         val result = adresseoppdatering.oppdaterUtenlandskKontaktadresse(sedHendelse(
-            rinaSakId = RINA_SAK_ID,
             avsenderNavn = "Dansk institusjon",
             avsenderLand = "DK",
-            rinaDokumentId = DOKUMENT_ID
         ))
         assertFalse(result)
     }
 
     @Test
     fun `Gitt en Sed med lik adresse i pdl som har dagens dato som gyldig fom dato saa sendes ingen oppdatering`() {
-        every { euxService.hentSed(eq(RINA_SAK_ID), eq(DOKUMENT_ID)) } returns
-                sed(
-                    FNR,
-                    EDDY_ADRESSE_I_SED
-                )
-        every { personService.hentPerson(NorskIdent(FNR)) } returns
+        every { euxService.hentSed(eq(SOME_RINA_SAK_ID), eq(SOME_DOKUMENT_ID)) } returns
+                sed(brukersAdresse = EDDY_ADRESSE_I_SED)
+
+        every { personService.hentPerson(NorskIdent(SOME_FNR)) } returns
                 personFraPDL(
-                    id = FNR,
                     utenlandskAdresse = EDDY_ADRESSE_FRA_PDL,
                     gyldigFraOgMed = LocalDateTime.now()
                 )
@@ -173,18 +171,37 @@ internal class AdresseoppdateringTest {
 
         val result = adresseoppdatering.oppdaterUtenlandskKontaktadresse(
             sedHendelse(
-                rinaSakId = RINA_SAK_ID,
                 avsenderNavn = "Svensk institusjon",
                 avsenderLand = EDDY_ADRESSE_LANDKODE,
-                rinaDokumentId = DOKUMENT_ID,
             ))
 
         assertFalse(result)
-
     }
 
     @Test
     fun `Gitt en sed med postboksadresse i gatefeltet som er lik postboksadresse i pdl saa oppdateres datoer`() {
+        every { euxService.hentSed(eq(SOME_RINA_SAK_ID), eq(SOME_DOKUMENT_ID)) } returns
+                sed(brukersAdresse = EDDY_ADRESSE_I_SED.copy(gate = "Postboks 543"))
+
+        every { personService.hentPerson(NorskIdent(SOME_FNR)) } returns
+                personFraPDL(
+                    utenlandskAdresse = EDDY_ADRESSE_FRA_PDL.copy(postboksNummerNavn = "Postboks 543", adressenavnNummer = null)
+                )
+
+        every { personMottakKlient.opprettPersonopplysning(any()) } returns true
+        val adresseoppdatering = Adresseoppdatering(personService, euxService, personMottakKlient, pdlFiltrering)
+
+        val result = adresseoppdatering.oppdaterUtenlandskKontaktadresse(sedHendelse(avsenderLand = EDDY_ADRESSE_LANDKODE))
+
+        assertTrue(result)
+
+        verify(atLeast = 1) { personMottakKlient.opprettPersonopplysning(eq(
+            pdlEndringOpplysning(
+                pdlAdresse = EDDY_ADRESSE_I_ENDRINGSMELDING.copy(postboksNummerNavn = "Postboks 543", adressenavnNummer = null),
+                kilde = "$SOME_UTENLANDSK_INSTITUSJON ($EDDY_ADRESSE_LANDKODE)",
+                gyldigFraOgMed = LocalDate.now(),
+                gyldigTilOgMed = LocalDate.now().plusYears(1)
+            ))) }
 
     }
 
@@ -194,9 +211,9 @@ internal class AdresseoppdateringTest {
     }
 
     private fun pdlEndringOpplysning(
-        id: String,
+        id: String = SOME_FNR,
         pdlAdresse: EndringsmeldingUtenlandskAdresse,
-        opplysningsId: String,
+        opplysningsId: String = "DummyOpplysningsId",
         kilde: String,
         gyldigFraOgMed: LocalDate,
         gyldigTilOgMed: LocalDate
@@ -222,10 +239,10 @@ internal class AdresseoppdateringTest {
     private fun sedHendelse(
         sektor: String = "P",
         bucType: String = "P_BUC_02",
-        rinaSakId: String,
-        avsenderNavn: String? = null,
+        rinaSakId: String = SOME_RINA_SAK_ID,
+        avsenderNavn: String? = SOME_UTENLANDSK_INSTITUSJON,
         avsenderLand: String? = null,
-        rinaDokumentId: String,
+        rinaDokumentId: String = SOME_DOKUMENT_ID,
         sedType: String = "P2100"
     ) = SedHendelse.fromJson(
         """{
@@ -244,7 +261,7 @@ internal class AdresseoppdateringTest {
                 }""".trimMargin()
     )
 
-    private fun sed(id: String = "65356", brukersAdresse: Adresse) = SED(
+    private fun sed(id: String = SOME_FNR, brukersAdresse: Adresse) = SED(
         type = SedType.P2100,
         sedGVer = null,
         sedVer = null,
@@ -294,7 +311,7 @@ internal class AdresseoppdateringTest {
     )
 
     private fun personFraPDL(
-        id: String,
+        id: String = SOME_FNR,
         utenlandskAdresse: UtenlandskAdresse,
         opplysningsId: String = "DummyOpplysningsId",
         gyldigFraOgMed: LocalDateTime = LocalDateTime.now().minusDays(10),
