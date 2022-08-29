9f1cf65b63ef14ffda172c5d1478704fc37c8090
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-08-29 10:49:47 +0200

    F skal likevel oppdatere adresse når kun beskyttet utland
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/Adressebeskyttelse.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/Adressebeskyttelse.kt
@@ -5,8 +5,7 @@ import no.nav.eessi.pensjon.personoppslag.pdl.model.AdressebeskyttelseGradering
 fun isAdressebeskyttet(list: List<AdressebeskyttelseGradering>): Boolean {
     val beskyttetStatus = setOf(
         AdressebeskyttelseGradering.FORTROLIG,
-        AdressebeskyttelseGradering.STRENGT_FORTROLIG,
-        AdressebeskyttelseGradering.STRENGT_FORTROLIG_UTLAND
+        AdressebeskyttelseGradering.STRENGT_FORTROLIG
     )
     return (list intersect beskyttetStatus).isNotEmpty()
 }
--- src/test/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/AdressebeskyttelseTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/AdressebeskyttelseTest.kt
@@ -14,14 +14,17 @@ class AdressebeskyttelseTest {
     fun `Adressebeskyttet`() {
         assertTrue(isAdressebeskyttet(listOf(FORTROLIG)))
         assertTrue(isAdressebeskyttet(listOf(STRENGT_FORTROLIG)))
-        assertTrue(isAdressebeskyttet(listOf(STRENGT_FORTROLIG_UTLAND)))
-        assertTrue(isAdressebeskyttet(listOf(UGRADERT, STRENGT_FORTROLIG_UTLAND)))
+        assertTrue(isAdressebeskyttet(listOf(STRENGT_FORTROLIG_UTLAND, STRENGT_FORTROLIG)))
+        assertTrue(isAdressebeskyttet(listOf(FORTROLIG, STRENGT_FORTROLIG_UTLAND)))
+        assertTrue(isAdressebeskyttet(listOf(UGRADERT, STRENGT_FORTROLIG)))
+        assertTrue(isAdressebeskyttet(listOf(UGRADERT, FORTROLIG)))
     }
 
     @Test
     fun `Ikke adressebeskyttet`() {
         assertFalse(isAdressebeskyttet(listOf()))
         assertFalse(isAdressebeskyttet(listOf(UGRADERT)))
+        assertFalse(isAdressebeskyttet(listOf(STRENGT_FORTROLIG_UTLAND)))
         assertFalse(isAdressebeskyttet(listOf(UGRADERT, UGRADERT)))
     }
 }
