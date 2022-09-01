1ed76ff0c25c9e0500a6a50b4ff3f0f7945e0306
mariampervez <mariam.pervez@nav.no>
2022-09-01 15:41:42 +0200

    r renamet variable
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Kjetil JD <kjetil.jorgensen-dahl@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/AdresseValidering.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/AdresseValidering.kt
@@ -4,25 +4,25 @@ class AdresseValidering {
 
     val maaInneholdeMinstEnBokstav = Regex(".*\\p{L}+.*")
     val maaInneholdeMinstEnBokstavEllerEtTall = Regex(".*[\\p{L}0-9]+.*")
-    val ugyldigPostInfo = listOf("postboks", "postb", "postbox", "p.b", "po.box")
-    val ugyldigFeltInfo = listOf("ukjent", "vet ikke", "nn")
+    val postboksFraser = listOf("postboks", "postb", "postbox", "p.b", "po.box")
+    val ukjentFraser = listOf("ukjent", "vet ikke", "nn")
 
     fun erGyldigAdressenavnNummerEllerBygningEtg(tekst: String): Boolean {
-        if (ugyldigPostInfo.any { tekst.contains(it) }) return false
-        if (ugyldigFeltInfo.any { tekst.contains(it) }) return false
         if (!tekst.matches(maaInneholdeMinstEnBokstav)) return false
+        if (postboksFraser.any { tekst.contains(it) }) return false
+        if (ukjentFraser.any { tekst.contains(it) }) return false
         return true
     }
 
     fun erGyldigByStedEllerRegion(tekst: String): Boolean {
-        if (ugyldigFeltInfo.any { tekst.contains(it) }) return false
         if (!tekst.matches(maaInneholdeMinstEnBokstav)) return false
+        if (ukjentFraser.any { tekst.contains(it) }) return false
         return true
     }
 
     fun erGyldigPostKode(tekst: String): Boolean {
-        if (ugyldigFeltInfo.any { tekst.contains(it) }) return false
         if (!tekst.matches(maaInneholdeMinstEnBokstavEllerEtTall)) return false
+        if (ukjentFraser.any { tekst.contains(it) }) return false
         return true
     }
 
