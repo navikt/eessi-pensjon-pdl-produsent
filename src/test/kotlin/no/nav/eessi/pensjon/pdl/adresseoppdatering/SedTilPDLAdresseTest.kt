08c2a8fa622f4eab9e7e36a7f1473d8b8bbd4f1e
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-09-02 12:45:26 +0200

    t rename file

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/SedTilPDLAdresseTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/SedTilPDLAdresseTest.kt
@@ -16,7 +16,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import java.time.LocalDate
 
-class SedTilPDLAdresseTest2 {
+class SedTilPDLAdresseTest {
 
     var kodeverkClient: KodeverkClient = mockk(relaxed = true)
 
