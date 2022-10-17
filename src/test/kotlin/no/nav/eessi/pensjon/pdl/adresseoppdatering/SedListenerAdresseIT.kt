9e2027527fbbafdcdaf961e9132ace8a872099b1
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-10-17 23:30:17 +0200

    t - Flytter hjelpe-metoder til companion object

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/SedListenerAdresseIT.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/SedListenerAdresseIT.kt
@@ -76,107 +76,109 @@ class SedListenerAdresseIT : IntegrationBase() {
         verify(exactly = 1) { personMottakKlient.opprettPersonopplysning(any()) }
     }
 
-    private fun enSedHendelse(): SedHendelse {
-        val sedHendelse: SedHendelse = SedHendelse(
-            sektorKode = "P",
-            bucType = BucType.P_BUC_01,
-            sedType = SedType.P2100,
-            rinaSakId = "74389487",
-            rinaDokumentId = "743982",
-            rinaDokumentVersjon = "1",
-            avsenderNavn = "Svensk institusjon",
-            avsenderLand = "SE"
-        )
-        return sedHendelse
-    }
-
-    private fun enSedFraEux(fnr: String) = SED(
-        type = SedType.P2100,
-        sedGVer = null,
-        sedVer = null,
-        nav = Nav(
-            eessisak = null,
-            bruker = Bruker(
-                mor = null,
-                far = null,
-                person = Person(
-                    pin = listOf(
-                        null ?: PinItem(
-                            identifikator = fnr,
-                            land = "NO"
-                        )
+    companion object {
+        fun enSedFraEux(fnr: String) = SED(
+            type = SedType.P2100,
+            sedGVer = null,
+            sedVer = null,
+            nav = Nav(
+                eessisak = null,
+                bruker = Bruker(
+                    mor = null,
+                    far = null,
+                    person = Person(
+                        pin = listOf(
+                            null ?: PinItem(
+                                identifikator = fnr,
+                                land = "NO"
+                            )
+                        ),
+                        pinland = null,
+                        statsborgerskap = null,
+                        etternavn = null,
+                        etternavnvedfoedsel = null,
+                        fornavn = null,
+                        fornavnvedfoedsel = null,
+                        tidligerefornavn = null,
+                        tidligereetternavn = null,
+                        kjoenn = null,
+                        foedested = null,
+                        foedselsdato = null,
+                        sivilstand = null,
+                        relasjontilavdod = null,
+                        rolle = null
                     ),
-                    pinland = null,
-                    statsborgerskap = null,
-                    etternavn = null,
-                    etternavnvedfoedsel = null,
-                    fornavn = null,
-                    fornavnvedfoedsel = null,
-                    tidligerefornavn = null,
-                    tidligereetternavn = null,
-                    kjoenn = null,
-                    foedested = null,
-                    foedselsdato = null,
-                    sivilstand = null,
-                    relasjontilavdod = null,
-                    rolle = null
-                ),
-                adresse = Adresse(
-                    gate = "Nedoverbakken 2",
-                    bygning = null,
-                    by = "Tulleby",
-                    postnummer = "SE-48940",
-                    region = "Brannland",
-                    land = "SE",
-                    kontaktpersonadresse = null,
-                ),
-                arbeidsforhold = null,
-                bank = null
-            )
-        ),
-        pensjon = Pensjon()
-    )
-
-    private fun enPersonFraPDL(fnr: String) = no.nav.eessi.pensjon.personoppslag.pdl.model.Person(
-        identer = listOf(
-            IdentInformasjon(
-                ident = fnr,
-                gruppe = IdentGruppe.FOLKEREGISTERIDENT
-            )
-        ),
-        navn = null,
-        adressebeskyttelse = listOf(),
-        bostedsadresse = null,
-        oppholdsadresse = null,
-        statsborgerskap = listOf(),
-        foedsel = null,
-        geografiskTilknytning = null,
-        kjoenn = null,
-        doedsfall = null,
-        forelderBarnRelasjon = listOf(),
-        sivilstand = listOf(),
-        kontaktadresse = Kontaktadresse(
-            coAdressenavn = "c/o Anund",
-            folkeregistermetadata = null,
-            gyldigFraOgMed = LocalDateTime.now().minusDays(5),
-            gyldigTilOgMed = LocalDateTime.now().plusDays(5),
-            metadata = Metadata(
-                endringer = emptyList(),
-                historisk = false,
-                master = "",
-                opplysningsId = "OpplysningsId"
+                    adresse = Adresse(
+                        gate = "Nedoverbakken 2",
+                        bygning = null,
+                        by = "Tulleby",
+                        postnummer = "SE-48940",
+                        region = "Brannland",
+                        land = "SE",
+                        kontaktpersonadresse = null,
+                    ),
+                    arbeidsforhold = null,
+                    bank = null
+                )
             ),
-            type = KontaktadresseType.Utland,
-            utenlandskAdresse = UtenlandskAdresse(
-                adressenavnNummer = "EddyRoad",
-                bygningEtasjeLeilighet = "EddyHouse",
-                bySted = "EddyCity",
-                postkode = "111",
-                regionDistriktOmraade = "Stockholm",
-                landkode = "SWE"
+            pensjon = Pensjon()
+        )
+
+        fun enSedHendelse(): SedHendelse {
+            val sedHendelse: SedHendelse = SedHendelse(
+                sektorKode = "P",
+                bucType = BucType.P_BUC_01,
+                sedType = SedType.P2100,
+                rinaSakId = "74389487",
+                rinaDokumentId = "743982",
+                rinaDokumentVersjon = "1",
+                avsenderNavn = "Svensk institusjon",
+                avsenderLand = "SE"
             )
-        ),
-        kontaktinformasjonForDoedsbo = null,
-        utenlandskIdentifikasjonsnummer = listOf()
-    )
+            return sedHendelse
+        }
+
+        fun enPersonFraPDL(fnr: String) = no.nav.eessi.pensjon.personoppslag.pdl.model.Person(
+            identer = listOf(
+                IdentInformasjon(
+                    ident = fnr,
+                    gruppe = IdentGruppe.FOLKEREGISTERIDENT
+                )
+            ),
+            navn = null,
+            adressebeskyttelse = listOf(),
+            bostedsadresse = null,
+            oppholdsadresse = null,
+            statsborgerskap = listOf(),
+            foedsel = null,
+            geografiskTilknytning = null,
+            kjoenn = null,
+            doedsfall = null,
+            forelderBarnRelasjon = listOf(),
+            sivilstand = listOf(),
+            kontaktadresse = Kontaktadresse(
+                coAdressenavn = "c/o Anund",
+                folkeregistermetadata = null,
+                gyldigFraOgMed = LocalDateTime.now().minusDays(5),
+                gyldigTilOgMed = LocalDateTime.now().plusDays(5),
+                metadata = Metadata(
+                    endringer = emptyList(),
+                    historisk = false,
+                    master = "",
+                    opplysningsId = "OpplysningsId"
+                ),
+                type = KontaktadresseType.Utland,
+                utenlandskAdresse = UtenlandskAdresse(
+                    adressenavnNummer = "EddyRoad",
+                    bygningEtasjeLeilighet = "EddyHouse",
+                    bySted = "EddyCity",
+                    postkode = "111",
+                    regionDistriktOmraade = "Stockholm",
+                    landkode = "SWE"
+                )
+            ),
+            kontaktinformasjonForDoedsbo = null,
+            utenlandskIdentifikasjonsnummer = listOf()
+        )
+    }
 }
