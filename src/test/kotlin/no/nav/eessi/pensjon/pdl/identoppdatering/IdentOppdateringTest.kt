2fcb4a27e6788ed74cb6a741f82b6a8cfbef16c1
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-11-01 18:17:50 +0100

    t - Fjerner object mother
    Dersom to tester trenger samme inndataoppbygning så tester de samme ting.

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/identoppdatering/IdentOppdateringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/identoppdatering/IdentOppdateringTest.kt
@@ -6,43 +6,60 @@ import io.mockk.verify
 import no.nav.eessi.pensjon.eux.EuxService
 import no.nav.eessi.pensjon.eux.UtenlandskId
 import no.nav.eessi.pensjon.eux.model.SedType
+import no.nav.eessi.pensjon.eux.model.buc.BucType
+import no.nav.eessi.pensjon.eux.model.sed.Adresse
+import no.nav.eessi.pensjon.eux.model.sed.Bruker
+import no.nav.eessi.pensjon.eux.model.sed.Nav
+import no.nav.eessi.pensjon.eux.model.sed.Pensjon
 import no.nav.eessi.pensjon.eux.model.sed.PinItem
+import no.nav.eessi.pensjon.eux.model.sed.SED
 import no.nav.eessi.pensjon.kodeverk.KodeverkClient
 import no.nav.eessi.pensjon.models.EndringsmeldingUID
 import no.nav.eessi.pensjon.models.PdlEndringOpplysning
 import no.nav.eessi.pensjon.models.Personopplysninger
+import no.nav.eessi.pensjon.models.SedHendelse
 import no.nav.eessi.pensjon.oppgave.OppgaveHandler
 import no.nav.eessi.pensjon.pdl.identoppdatering.IdentOppdatering.IngenOppdatering
 import no.nav.eessi.pensjon.pdl.identoppdatering.IdentOppdatering.Oppdatering
 import no.nav.eessi.pensjon.pdl.identoppdatering.IdentOppdatering.Oppgave
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.identifisertPerson
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.personFraPDL
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.sed
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.sedHendelse
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.utenlandskIdentifikasjonsnummer
 import no.nav.eessi.pensjon.pdl.validering.LandspesifikkValidering
+import no.nav.eessi.pensjon.personidentifisering.IdentifisertPerson
+import no.nav.eessi.pensjon.personidentifisering.Relasjon
+import no.nav.eessi.pensjon.personidentifisering.SEDPersonRelasjon
+import no.nav.eessi.pensjon.personoppslag.Fodselsnummer
 import no.nav.eessi.pensjon.personoppslag.pdl.PersonService
 import no.nav.eessi.pensjon.personoppslag.pdl.PersonoppslagException
+import no.nav.eessi.pensjon.personoppslag.pdl.model.AdressebeskyttelseGradering
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Doedsfall
 import no.nav.eessi.pensjon.personoppslag.pdl.model.Endringstype
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Folkeregistermetadata
 import no.nav.eessi.pensjon.personoppslag.pdl.model.IdentGruppe
 import no.nav.eessi.pensjon.personoppslag.pdl.model.IdentInformasjon
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Kontaktadresse
+import no.nav.eessi.pensjon.personoppslag.pdl.model.KontaktadresseType
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Metadata
 import no.nav.eessi.pensjon.personoppslag.pdl.model.NorskIdent
 import no.nav.eessi.pensjon.personoppslag.pdl.model.Opplysningstype
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Person
+import no.nav.eessi.pensjon.personoppslag.pdl.model.UtenlandskAdresse
+import no.nav.eessi.pensjon.personoppslag.pdl.model.UtenlandskIdentifikasjonsnummer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 private const val FNR = "11067122781"
 private const val DNR = "51077403071"
 private const val SVENSK_FNR = "512020-1234"
 private const val FINSK_FNR = "130177-308T"
-
+private const val SOME_FNR = "1234567799"
 private const val AKTOERID = "32165498732"
 
-internal class IdentOppdateringTest {
+private class IdentOppdateringTest {
 
     var euxService: EuxService = mockk(relaxed = true)
     var kodeverkClient: KodeverkClient = mockk(relaxed = true)
@@ -412,14 +429,14 @@ internal class IdentOppdateringTest {
 
     @Test
     fun `Gitt at vi har en SED med norsk fnr som skal oppdateres til pdl, der PDL har en aktoerid inne i PDL saa skal Oppdateringsmeldingen til PDL ha norsk FNR og ikke aktoerid`() {
-        val identifisertPerson= TestDataPDL.identifisertPerson(uidFraPdl = listOf(TestDataPDL.utenlandskIdentifikasjonsnummer(SVENSK_FNR)))
+        val identifisertPerson= identifisertPerson(uidFraPdl = listOf(utenlandskIdentifikasjonsnummer(SVENSK_FNR)))
 
         every { personService.hentPerson(NorskIdent(FNR)) } returns
-                TestDataPDL.personFraPDL(id = FNR).copy(identer = listOf(IdentInformasjon("1234567891234", IdentGruppe.AKTORID), IdentInformasjon(FNR, IdentGruppe.FOLKEREGISTERIDENT)))
-                        .copy(utenlandskIdentifikasjonsnummer = listOf(TestDataPDL.utenlandskIdentifikasjonsnummer(FINSK_FNR).copy(utstederland = "FIN")))
+                personFraPDL(id = FNR).copy(identer = listOf(IdentInformasjon("1234567891234", IdentGruppe.AKTORID), IdentInformasjon(FNR, IdentGruppe.FOLKEREGISTERIDENT)))
+                        .copy(utenlandskIdentifikasjonsnummer = listOf(utenlandskIdentifikasjonsnummer(FINSK_FNR).copy(utstederland = "FIN")))
 
         every { euxService.hentSed(any(), any()) } returns
-                TestDataPDL.sed(
+                sed(
                     id = FNR, land = "NO", pinItem = listOf(
                         PinItem(identifikator = "5 12 020-1234", land = "SE"),
                         PinItem(identifikator = FNR, land = "NO")
@@ -431,7 +448,7 @@ internal class IdentOppdateringTest {
 
         assertEquals(
                 Oppdatering("Innsending av endringsmelding", pdlEndringsMelding(FNR, utstederland = "SWE")),
-                identoppdatering.oppdaterUtenlandskIdent(TestDataPDL.sedHendelse(avsenderLand = "SE"))
+                identoppdatering.oppdaterUtenlandskIdent(sedHendelse(avsenderLand = "SE"))
         )
 
     }
@@ -440,7 +457,7 @@ internal class IdentOppdateringTest {
     fun `Gitt at vi har en SED med norsk dnr som skal oppdateres til pdl, der PDL har et fnr inne så skal Oppdateringsmeldingen til PDL ha norsk FNR og ikke dnr`() {
         every { personService.hentPerson(NorskIdent(DNR)) } returns
                 personFraPDL(id = FNR).copy(identer = listOf(IdentInformasjon(FNR, IdentGruppe.FOLKEREGISTERIDENT)))
-                        .copy(utenlandskIdentifikasjonsnummer = listOf(TestDataPDL.utenlandskIdentifikasjonsnummer(FINSK_FNR).copy(utstederland = "FIN")))
+                        .copy(utenlandskIdentifikasjonsnummer = listOf(utenlandskIdentifikasjonsnummer(FINSK_FNR).copy(utstederland = "FIN")))
 
         every { euxService.hentSed(any(), any()) } returns
                 sed(
@@ -452,7 +469,7 @@ internal class IdentOppdateringTest {
 
         assertEquals(
                 Oppdatering("Innsending av endringsmelding", pdlEndringsMelding(FNR, utstederland = "SWE")),
-                identoppdatering.oppdaterUtenlandskIdent(TestDataPDL.sedHendelse(avsenderLand = "SE"))
+                identoppdatering.oppdaterUtenlandskIdent(sedHendelse(avsenderLand = "SE"))
         )
     }
 
@@ -477,4 +494,136 @@ internal class IdentOppdateringTest {
             )
         )
 
+    private fun identifisertPerson(
+        erDoed: Boolean = false, uidFraPdl: List<UtenlandskIdentifikasjonsnummer> = emptyList()
+    ) = IdentifisertPerson(
+        fnr = Fodselsnummer.fra(FNR),
+        uidFraPdl = uidFraPdl,
+        aktoerId = "123456789351",
+        landkode = null,
+        geografiskTilknytning = null,
+        harAdressebeskyttelse = erDoed,
+        personListe = null,
+        personRelasjon = SEDPersonRelasjon(
+            relasjon = Relasjon.ANNET,
+            fnr = Fodselsnummer.fra(FNR),
+            rinaDocumentId = "12345"
+        ),
+        erDoed = erDoed,
+        kontaktAdresse = null,
+    )
+
+    private fun utenlandskIdentifikasjonsnummer(fnr: String) = UtenlandskIdentifikasjonsnummer(
+        identifikasjonsnummer = fnr,
+        utstederland = "POL",
+        opphoert = false,
+        folkeregistermetadata = Folkeregistermetadata(
+            gyldighetstidspunkt = LocalDateTime.now(),
+            ajourholdstidspunkt = LocalDateTime.now(),
+        ),
+        metadata = metadata()
+    )
+
+    private fun metadata() = Metadata(
+        endringer = emptyList(),
+        historisk = false,
+        master = "PDL",
+        opplysningsId = "opplysningsId"
+    )
+
+    private fun sedHendelse(
+        bucType: BucType = BucType.P_BUC_01,
+        sedType: SedType = SedType.P2000,
+        avsenderLand: String = "SE",
+        avsenderNavn: String? = "Utenlandsk institusjon"
+    ) = SedHendelse(
+        sektorKode = "P",
+        avsenderLand = avsenderLand,
+        bucType = bucType,
+        rinaSakId = "123456479867",
+        rinaDokumentId = "SOME_DOKUMENT_ID",
+        rinaDokumentVersjon = "SOME_RINADOKUMENT_VERSION",
+        sedType = sedType,
+        avsenderNavn = avsenderNavn
+    )
+
+    private fun sed(id: String = SOME_FNR,
+                     brukersAdresse: Adresse? = null,
+                     land: String?,
+                     pinItem: List<PinItem>? = listOf(PinItem(land = land, identifikator = id)) ) = SED(
+        type = SedType.P2000,
+        sedGVer = null,
+        sedVer = null,
+        nav = Nav(
+            eessisak = null,
+            bruker = Bruker(
+                mor = null,
+                far = null,
+                person = no.nav.eessi.pensjon.eux.model.sed.Person(
+                    pin = pinItem,
+                    pinland = null,
+                    statsborgerskap = null,
+                    etternavn = null,
+                    etternavnvedfoedsel = null,
+                    fornavn = null,
+                    fornavnvedfoedsel = null,
+                    tidligerefornavn = null,
+                    tidligereetternavn = null,
+                    kjoenn = null,
+                    foedested = null,
+                    foedselsdato = null,
+                    sivilstand = null,
+                    relasjontilavdod = null,
+                    rolle = null
+                ),
+                adresse = brukersAdresse,
+                arbeidsforhold = null,
+                bank = null
+            )
+        ),
+        pensjon = Pensjon()
+    )
+
+    private fun personFraPDL(
+        id: String = SOME_FNR,
+        adressebeskyttelse: List<AdressebeskyttelseGradering> = listOf(),
+        utenlandskAdresse: UtenlandskAdresse? = null,
+        opplysningsId: String = "DummyOpplysningsId",
+        gyldigFraOgMed: LocalDateTime = LocalDateTime.now().minusDays(10),
+        gyldigTilOgMed: LocalDateTime = LocalDateTime.now().plusDays(10),
+        doedsdato: LocalDate? = null
+    ): Person = Person(
+        identer = listOf(IdentInformasjon(id, IdentGruppe.FOLKEREGISTERIDENT)),
+        navn = null,
+        adressebeskyttelse = adressebeskyttelse,
+        bostedsadresse = null,
+        oppholdsadresse = null,
+        statsborgerskap = listOf(),
+        foedsel = null,
+        geografiskTilknytning = null,
+        kjoenn = null,
+        doedsfall = Doedsfall(
+            doedsdato = doedsdato,
+            folkeregistermetadata = null,
+            metadata= metadata()
+        ),
+        forelderBarnRelasjon = listOf(),
+        sivilstand = listOf(),
+        kontaktadresse = Kontaktadresse(
+            coAdressenavn = "c/o Anund",
+            folkeregistermetadata = null,
+            gyldigFraOgMed = gyldigFraOgMed,
+            gyldigTilOgMed = gyldigTilOgMed,
+            metadata = Metadata(
+                endringer = emptyList(),
+                historisk = false,
+                master = "",
+                opplysningsId = opplysningsId
+            ),
+            type = KontaktadresseType.Utland,
+            utenlandskAdresse = utenlandskAdresse
+        ),
+        kontaktinformasjonForDoedsbo = null,
+        utenlandskIdentifikasjonsnummer = listOf()
+    )
 }
--- src/test/kotlin/no/nav/eessi/pensjon/pdl/identoppdatering/SedHendelseIdentBehandlerTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/identoppdatering/SedHendelseIdentBehandlerTest.kt
@@ -5,20 +5,37 @@ import io.mockk.every
 import io.mockk.verify
 import no.nav.eessi.pensjon.eux.EuxService
 import no.nav.eessi.pensjon.eux.UtenlandskId
+import no.nav.eessi.pensjon.eux.model.SedType
+import no.nav.eessi.pensjon.eux.model.buc.BucType
+import no.nav.eessi.pensjon.eux.model.sed.Adresse
+import no.nav.eessi.pensjon.eux.model.sed.Bruker
+import no.nav.eessi.pensjon.eux.model.sed.Nav
+import no.nav.eessi.pensjon.eux.model.sed.Pensjon
 import no.nav.eessi.pensjon.eux.model.sed.PinItem
+import no.nav.eessi.pensjon.eux.model.sed.SED
 import no.nav.eessi.pensjon.kodeverk.KodeverkClient
+import no.nav.eessi.pensjon.models.SedHendelse
 import no.nav.eessi.pensjon.oppgave.OppgaveHandler
 import no.nav.eessi.pensjon.pdl.PersonMottakKlient
 import no.nav.eessi.pensjon.pdl.adresseoppdatering.SedListenerAdresseIT
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.identifisertPerson
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.personFraPDL
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.sed
-import no.nav.eessi.pensjon.pdl.identoppdatering.TestDataPDL.utenlandskIdentifikasjonsnummer
 import no.nav.eessi.pensjon.pdl.validering.LandspesifikkValidering
+import no.nav.eessi.pensjon.personidentifisering.IdentifisertPerson
+import no.nav.eessi.pensjon.personidentifisering.Relasjon
+import no.nav.eessi.pensjon.personidentifisering.SEDPersonRelasjon
+import no.nav.eessi.pensjon.personoppslag.Fodselsnummer
 import no.nav.eessi.pensjon.personoppslag.pdl.PersonService
+import no.nav.eessi.pensjon.personoppslag.pdl.model.AdressebeskyttelseGradering
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Doedsfall
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Folkeregistermetadata
 import no.nav.eessi.pensjon.personoppslag.pdl.model.IdentGruppe
 import no.nav.eessi.pensjon.personoppslag.pdl.model.IdentInformasjon
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Kontaktadresse
+import no.nav.eessi.pensjon.personoppslag.pdl.model.KontaktadresseType
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Metadata
 import no.nav.eessi.pensjon.personoppslag.pdl.model.NorskIdent
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Person
+import no.nav.eessi.pensjon.personoppslag.pdl.model.UtenlandskAdresse
+import no.nav.eessi.pensjon.personoppslag.pdl.model.UtenlandskIdentifikasjonsnummer
 import no.nav.eessi.pensjon.utils.toJson
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -30,11 +47,13 @@ import org.springframework.stereotype.Component
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import org.springframework.web.client.HttpClientErrorException
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 private const val FNR = "11067122781"
 private const val SVENSK_FNR = "512020-1234"
 private const val FINSK_FNR = "130177-308T"
-
+private const val SOME_FNR = "1234567799"
 private const val SVERIGE = "SE"
 private const val NORGE = "NO"
 
@@ -47,7 +66,7 @@ private const val NORGE = "NO"
     TestSedHendelseIdentBehandlerRetryConfig::class]
 )
 @EnableRetry
-internal class SedHendelseIdentBehandlerTest {
+private class SedHendelseIdentBehandlerTest {
 
     @MockkBean
     lateinit var euxService: EuxService
@@ -99,6 +118,139 @@ internal class SedHendelseIdentBehandlerTest {
         verify(exactly = 3) { personMottakKlient.opprettPersonopplysning(any()) }
 
     }
+
+    private fun identifisertPerson(
+        erDoed: Boolean = false, uidFraPdl: List<UtenlandskIdentifikasjonsnummer> = emptyList()
+    ) = IdentifisertPerson(
+        fnr = Fodselsnummer.fra(FNR),
+        uidFraPdl = uidFraPdl,
+        aktoerId = "123456789351",
+        landkode = null,
+        geografiskTilknytning = null,
+        harAdressebeskyttelse = erDoed,
+        personListe = null,
+        personRelasjon = SEDPersonRelasjon(
+            relasjon = Relasjon.ANNET,
+            fnr = Fodselsnummer.fra(FNR),
+            rinaDocumentId = "12345"
+        ),
+        erDoed = erDoed,
+        kontaktAdresse = null,
+    )
+
+    private fun utenlandskIdentifikasjonsnummer(fnr: String) = UtenlandskIdentifikasjonsnummer(
+        identifikasjonsnummer = fnr,
+        utstederland = "POL",
+        opphoert = false,
+        folkeregistermetadata = Folkeregistermetadata(
+            gyldighetstidspunkt = LocalDateTime.now(),
+            ajourholdstidspunkt = LocalDateTime.now(),
+        ),
+        metadata = metadata()
+    )
+
+    private fun metadata() = Metadata(
+        endringer = emptyList(),
+        historisk = false,
+        master = "PDL",
+        opplysningsId = "opplysningsId"
+    )
+
+    private fun sedHendelse(
+        bucType: BucType = BucType.P_BUC_01,
+        sedType: SedType = SedType.P2000,
+        avsenderLand: String = "SE",
+        avsenderNavn: String? = "Utenlandsk institusjon"
+    ) = SedHendelse(
+        sektorKode = "P",
+        avsenderLand = avsenderLand,
+        bucType = bucType,
+        rinaSakId = "123456479867",
+        rinaDokumentId = "SOME_DOKUMENT_ID",
+        rinaDokumentVersjon = "SOME_RINADOKUMENT_VERSION",
+        sedType = sedType,
+        avsenderNavn = avsenderNavn
+    )
+
+    private fun sed(id: String = SOME_FNR,
+                     brukersAdresse: Adresse? = null,
+                     land: String?,
+                     pinItem: List<PinItem>? = listOf(PinItem(land = land, identifikator = id)) ) = SED(
+        type = SedType.P2000,
+        sedGVer = null,
+        sedVer = null,
+        nav = Nav(
+            eessisak = null,
+            bruker = Bruker(
+                mor = null,
+                far = null,
+                person = no.nav.eessi.pensjon.eux.model.sed.Person(
+                    pin = pinItem,
+                    pinland = null,
+                    statsborgerskap = null,
+                    etternavn = null,
+                    etternavnvedfoedsel = null,
+                    fornavn = null,
+                    fornavnvedfoedsel = null,
+                    tidligerefornavn = null,
+                    tidligereetternavn = null,
+                    kjoenn = null,
+                    foedested = null,
+                    foedselsdato = null,
+                    sivilstand = null,
+                    relasjontilavdod = null,
+                    rolle = null
+                ),
+                adresse = brukersAdresse,
+                arbeidsforhold = null,
+                bank = null
+            )
+        ),
+        pensjon = Pensjon()
+    )
+
+    private fun personFraPDL(
+        id: String = SOME_FNR,
+        adressebeskyttelse: List<AdressebeskyttelseGradering> = listOf(),
+        utenlandskAdresse: UtenlandskAdresse? = null,
+        opplysningsId: String = "DummyOpplysningsId",
+        gyldigFraOgMed: LocalDateTime = LocalDateTime.now().minusDays(10),
+        gyldigTilOgMed: LocalDateTime = LocalDateTime.now().plusDays(10),
+        doedsdato: LocalDate? = null
+    ): Person = Person(
+        identer = listOf(IdentInformasjon(id, IdentGruppe.FOLKEREGISTERIDENT)),
+        navn = null,
+        adressebeskyttelse = adressebeskyttelse,
+        bostedsadresse = null,
+        oppholdsadresse = null,
+        statsborgerskap = listOf(),
+        foedsel = null,
+        geografiskTilknytning = null,
+        kjoenn = null,
+        doedsfall = Doedsfall(
+            doedsdato = doedsdato,
+            folkeregistermetadata = null,
+            metadata= metadata()
+        ),
+        forelderBarnRelasjon = listOf(),
+        sivilstand = listOf(),
+        kontaktadresse = Kontaktadresse(
+            coAdressenavn = "c/o Anund",
+            folkeregistermetadata = null,
+            gyldigFraOgMed = gyldigFraOgMed,
+            gyldigTilOgMed = gyldigTilOgMed,
+            metadata = Metadata(
+                endringer = emptyList(),
+                historisk = false,
+                master = "",
+                opplysningsId = opplysningsId
+            ),
+            type = KontaktadresseType.Utland,
+            utenlandskAdresse = utenlandskAdresse
+        ),
+        kontaktinformasjonForDoedsbo = null,
+        utenlandskIdentifikasjonsnummer = listOf()
+    )
 }
 
 @Profile("retryConfigOverride")
