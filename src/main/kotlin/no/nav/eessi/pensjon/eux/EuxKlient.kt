ef574a2742f79cf676e7781e5087646155779e9c
mariampervez <mariam.pervez@nav.no>
2022-12-07 10:46:29 +0100

    r - Fjerner retryable fra eux-klienten da dette allerede kommer fra interceptor i resttemplate config
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/eux/EuxKlient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/EuxKlient.kt
@@ -5,12 +5,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
-import org.springframework.retry.annotation.Backoff
-import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
-import org.springframework.web.client.HttpClientErrorException
-import org.springframework.web.client.HttpStatusCodeException
-import org.springframework.web.client.ResourceAccessException
 import org.springframework.web.client.RestTemplate
 
 @Component
@@ -18,11 +13,6 @@ class EuxKlient(private val euxOAuthRestTemplate: RestTemplate) {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(EuxKlient::class.java) }
 
-    @Retryable(
-        include = [HttpStatusCodeException::class],
-        exclude = [HttpClientErrorException.NotFound::class],
-        backoff = Backoff(delay = 30000L, maxDelay = 3600000L, multiplier = 3.0)
-    )
     internal fun hentSedJson(rinaSakId: String, dokumentId: String): String? {
         logger.info("Henter SED for rinaSakId: $rinaSakId , dokumentId: $dokumentId")
 
@@ -36,10 +26,6 @@ class EuxKlient(private val euxOAuthRestTemplate: RestTemplate) {
     return exchange.body
     }
 
-    @Retryable(
-        include = [HttpStatusCodeException::class],
-        backoff = Backoff(delay = 30000L, maxDelay = 3600000L, multiplier = 3.0)
-    )
     internal fun hentBuc(rinaSakId: String): Buc? {
         logger.info("Henter BUC (RinaSakId: $rinaSakId)")
 
