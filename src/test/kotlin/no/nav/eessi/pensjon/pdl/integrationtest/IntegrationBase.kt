f597dbf604605ba290d46093d9d26730931430b8
Daniel Skarpås <dskarpas@gmail.com>
2022-12-15 10:13:46 +0100

    U - spring-boot '2.7.5' ->'3.0.0', httpclient5 4 -> 5 (grunnet oppgradering av SB), ep-kodeverk:3.1.40 -> 3.1.43

--- src/test/kotlin/no/nav/eessi/pensjon/pdl/integrationtest/IntegrationBase.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/integrationtest/IntegrationBase.kt
@@ -14,9 +14,12 @@ import no.nav.eessi.pensjon.personoppslag.pdl.PersonService
 import no.nav.eessi.pensjon.utils.mapJsonToAny
 import no.nav.eessi.pensjon.utils.toJson
 import no.nav.eessi.pensjon.utils.typeRefs
-import org.apache.http.conn.ssl.NoopHostnameVerifier
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClients
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
+import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder
+import org.apache.hc.client5.http.io.HttpClientConnectionManager
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder
 import org.apache.http.ssl.SSLContexts
 import org.apache.http.ssl.TrustStrategy
 import org.apache.kafka.clients.consumer.ConsumerConfig
@@ -218,15 +221,21 @@ abstract class IntegrationBase {
 
         @Bean
         fun opprettRestTemplate(): RestTemplate {
+
             val acceptingTrustStrategy = TrustStrategy { _: Array<X509Certificate?>?, _: String? -> true }
 
-            val sslContext: SSLContext = SSLContexts.custom()
+            val sslcontext: SSLContext = SSLContexts.custom()
                 .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build()
+            val sslSocketFactory: SSLConnectionSocketFactory = SSLConnectionSocketFactoryBuilder.create()
+                .setSslContext(sslcontext)
+                .build()
+            val cm: HttpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .setSSLSocketFactory(sslSocketFactory)
                 .build()
 
             val httpClient: CloseableHttpClient = HttpClients.custom()
-                .setSSLContext(sslContext)
-                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .setConnectionManager(cm)
                 .build()
 
             val customRequestFactory = HttpComponentsClientHttpRequestFactory()
