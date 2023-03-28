49581f2848347d6f715bb388150b6bc31ba9d5ee
Daniel Skarpås <dskarpas@gmail.com>
2023-03-28 23:22:50 +0200

    T - Oppsett for config test

--- src/test/kotlin/no/nav/eessi/pensjon/EessiApplicationConfigTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/EessiApplicationConfigTest.kt
@@ -6,28 +6,26 @@ import no.nav.eessi.pensjon.config.RestTemplateConfig
 import no.nav.eessi.pensjon.eux.klient.EuxKlientAsSystemUser
 import no.nav.eessi.pensjon.gcp.GcpStorageService
 import no.nav.eessi.pensjon.klienter.norg2.Norg2Klient
-import no.nav.eessi.pensjon.klienter.norg2.Norg2Service
+import no.nav.eessi.pensjon.kodeverk.KodeverkClient
 import no.nav.eessi.pensjon.pdl.PersonMottakKlient
-import no.nav.eessi.pensjon.pdl.adresseoppdatering.SedListenerAdresse
-import no.nav.eessi.pensjon.pdl.integrationtest.KafkaTestConfig
 import no.nav.eessi.pensjon.personoppslag.pdl.PersonClient
 import no.nav.eessi.pensjon.personoppslag.pdl.PersonService
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.web.client.RestTemplate
 
-@SpringBootTest(classes = [RestTemplateConfig::class, KafkaTestConfig::class,UnsecuredWebMvcTestLauncher::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles(profiles = ["unsecured-webmvctest"])
+@SpringBootTest(classes = [RestTemplateConfig::class, UnsecuredWebMvcTestLauncher::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = [ "excludeKodeverk","unsecured-webmvctest", "integrationtest"])
 @EmbeddedKafka
 @EnableMockOAuth2Server
 @AutoConfigureMockMvc
 @MockkBeans(
+    //rest
     MockkBean(name = "prefillOAuthTemplate", classes = [RestTemplate::class]),
     MockkBean(name = "euxSystemRestTemplate", classes = [RestTemplate::class]),
     MockkBean(name = "safRestOidcRestTemplate", classes = [RestTemplate::class]),
@@ -35,19 +33,23 @@ import org.springframework.web.client.RestTemplate
     MockkBean(name = "euxNavIdentRestTemplate", classes = [RestTemplate::class]),
     MockkBean(name = "restEuxTemplate", classes = [RestTemplate::class]),
     MockkBean(name = "safGraphQlOidcRestTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "kodeverkRestTemplate", classes = [RestTemplate::class]),
+    //service / clients
     MockkBean(name = "personService", classes = [PersonService::class]),
     MockkBean(name = "euxKlient", classes = [EuxKlientAsSystemUser::class]),
     MockkBean(name = "gcpStorageService", classes = [GcpStorageService::class]),
     MockkBean(name = "norg2Klient", classes = [Norg2Klient::class]),
-    MockkBean(name = "personMottakKlient", classes = [PersonMottakKlient::class])
-)
-class EessiTokenValdationTest {
+    MockkBean(name = "personMottakKlient", classes = [PersonMottakKlient::class]),
+    MockkBean(name = "kodeverkClient", classes = [KodeverkClient::class]),
+    MockkBean(name = "personClient", classes = [PersonClient::class]),
+    //kafka
+    MockkBean(name = "sedKafkaListenerContainerFactory", classes = [ConcurrentKafkaListenerContainerFactory::class], relaxed = true),
 
-    @Autowired
-    protected lateinit var mockMvc: MockMvc
+)
+class EessiApplicationConfigTest {
 
     @Test
     fun `contextTest`(){
-        //alt er vel om vi kommer hit
+        println("alt er vel om vi kommer hit")
     }
 }
