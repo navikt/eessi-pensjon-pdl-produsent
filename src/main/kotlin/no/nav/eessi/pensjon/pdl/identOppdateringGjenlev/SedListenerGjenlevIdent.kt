966e7c4f19eb1422c87815cee44b83b7148ba19e
Daniel Skarpås <dskarpas@gmail.com>
2023-05-10 09:47:05 +0200

    B - Kommenterer ut behandling av gjenlev mens det jobbes med løsningen

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/identOppdateringGjenlev/SedListenerGjenlevIdent.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/identOppdateringGjenlev/SedListenerGjenlevIdent.kt
@@ -40,7 +40,7 @@ class SedListenerGjenlevIdent(
             consumeIncomingSed.measure {
                 logger.info("SedGjenlevMottatt i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
                 try {
-                    behandleIdentHendelse.behandlenGjenlevHendelse(hendelse)
+//                    behandleIdentHendelse.behandlenGjenlevHendelse(hendelse)
                     acknowledgment.acknowledge()
                     logger.info("Acket sedGjenlevMottatt melding med offset: ${cr.offset()} i partisjon ${cr.partition()}")
                     latch.countDown()
