d034594189677093ef4a106929199a7b51f0d085
mariampervez <mariam.pervez@nav.no>
2023-06-13 14:24:55 +0200

    F - Aktiverer uid oppdatering av gjenlevende
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/identOppdateringGjenlev/SedListenerGjenlevIdent.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/identOppdateringGjenlev/SedListenerGjenlevIdent.kt
@@ -5,17 +5,19 @@ import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
 import java.util.*
 import java.util.concurrent.CountDownLatch
 import javax.annotation.PostConstruct
 
 @Service
 class SedListenerGjenlevIdent(
-        private val behandleIdentHendelse: SedHendelseGjenlevIdentBehandler,
-        @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()
+    private val behandleIdentHendelse: SedHendelseGjenlevIdentBehandler,
+    @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()
 ) {
 
     private val logger = LoggerFactory.getLogger(SedListenerGjenlevIdent::class.java)
@@ -40,15 +42,19 @@ class SedListenerGjenlevIdent(
             consumeIncomingSed.measure {
                 logger.info("SedGjenlevMottatt i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
                 try {
-//                    behandleIdentHendelse.behandlenGjenlevHendelse(hendelse)
-                    acknowledgment.acknowledge()
+                    behandleIdentHendelse.behandlenGjenlevHendelse(hendelse)
                     logger.info("Acket sedGjenlevMottatt melding med offset: ${cr.offset()} i partisjon ${cr.partition()}")
                     latch.countDown()
+                } catch (ex: HttpClientErrorException) {
+                    if (ex.statusCode == HttpStatus.LOCKED)
+                        logger.error("Det pågår allerede en adresseoppdatering på bruker", ex)
+                    else throw ex
                 } catch (ex: Exception) {
                     logger.error("Noe gikk galt under behandling av SED-hendelse", ex)
                     secureLogger.info("Noe gikk galt under behandling av SED-hendelse:\n$hendelse")
                     throw ex
                 }
+                acknowledgment.acknowledge()
             }
         }
     }
