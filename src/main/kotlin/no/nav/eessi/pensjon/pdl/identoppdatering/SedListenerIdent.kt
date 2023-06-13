54ec32bb0d9865a0e58eb34f7cf3f6198fccff6c
mariampervez <mariam.pervez@nav.no>
2023-06-13 13:44:32 +0200

    f - Legger til ack etter 3 retry på exception med HttpCode.LOCKED på UID identOppdatering
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/identoppdatering/SedListenerIdent.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/identoppdatering/SedListenerIdent.kt
@@ -5,17 +5,19 @@ import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
 import java.util.*
 import java.util.concurrent.CountDownLatch
 import javax.annotation.PostConstruct
 
 @Service
 class SedListenerIdent(
-        private val behandleIdentHendelse: SedHendelseIdentBehandler,
-        @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()
+    private val behandleIdentHendelse: SedHendelseIdentBehandler,
+    @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()
 ) {
 
     private val logger = LoggerFactory.getLogger(SedListenerIdent::class.java)
@@ -40,20 +42,23 @@ class SedListenerIdent(
             consumeIncomingSed.measure {
                 logger.info("SedMottatt i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
                 try {
-                    if(cr.offset() in listOf(518691L, 519143L)){
+                    if (cr.offset() in listOf(518691L, 519143L)) {
                         logger.warn("Hopper over offset: ${cr.offset()} grunnet feil ved henting av vedlegg...")
-                    }
-                    else {
+                    } else {
                         behandleIdentHendelse.behandle(hendelse)
                     }
-                    acknowledgment.acknowledge()
                     logger.info("Acket sedMottatt melding med offset: ${cr.offset()} i partisjon ${cr.partition()}")
                     latch.countDown()
+                } catch (ex: HttpClientErrorException) {
+                    if (ex.statusCode == HttpStatus.LOCKED)
+                        logger.error("Det pågår allerede en adresseoppdatering på bruker", ex)
+                    else throw ex
                 } catch (ex: Exception) {
                     logger.error("Noe gikk galt under behandling av SED-hendelse", ex)
                     secureLogger.info("Noe gikk galt under behandling av SED-hendelse:\n$hendelse")
                     throw ex
                 }
+                acknowledgment.acknowledge()
             }
         }
     }
---
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/identoppdatering/SedListenerTest.kt
@@ -0,0 +1,97 @@
+package no.nav.eessi.pensjon.pdl.identoppdatering
+
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.eessi.pensjon.eux.model.BucType
+import no.nav.eessi.pensjon.eux.model.SedHendelse
+import no.nav.eessi.pensjon.eux.model.SedType
+import no.nav.eessi.pensjon.oppgave.OppgaveHandler
+import no.nav.eessi.pensjon.pdl.PersonMottakKlient
+import no.nav.eessi.pensjon.pdl.integrationtest.IntegrationBase
+import no.nav.eessi.pensjon.pdl.integrationtest.KafkaTestConfig
+import no.nav.eessi.pensjon.utils.toJson
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpStatus
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.retry.annotation.EnableRetry
+import org.springframework.stereotype.Component
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.web.client.HttpClientErrorException
+
+
+@SpringBootTest(classes = [
+    KafkaTestConfig::class,
+    IntegrationBase.TestConfig::class,
+    SedListenerIdent::class,
+    SedHendelseIdentBehandler::class,
+    SedListenerTestConfig::class,
+    SedHendelseIdentBehandlerRetryLogger::class
+])
+@ActiveProfiles("retryConfigOverride")
+@DirtiesContext
+@EnableRetry
+@EmbeddedKafka(
+    controlledShutdown = true,
+    topics = ["eessi-basis-sedMottatt-v1"]
+)
+class SedListenerTest {
+
+    @Autowired
+    private lateinit var sedListenerIdent: SedListenerIdent
+
+    @Autowired
+    private lateinit var sedHendelseIdentBehandler: SedHendelseIdentBehandler
+
+    @MockkBean
+    private lateinit var vurderIdentoppdatering: VurderIdentoppdatering
+
+    @MockkBean
+    private lateinit var personMottakKlient: PersonMottakKlient
+
+    @MockkBean
+    private lateinit var oppgaveHandler: OppgaveHandler
+
+    @BeforeEach
+    fun setUp() {
+        sedListenerIdent.initMetrics()
+    }
+
+    @Test
+    fun `Sjekk om brukers fnr stemmer overens med brukers fdato foer vi vuderer aa oppdatere UID på bruker`() {
+
+        val ack = mockk<Acknowledgment>()
+        justRun { ack.acknowledge() }
+
+        every { vurderIdentoppdatering.vurderUtenlandskIdent(any()) } throws HttpClientErrorException(HttpStatus.LOCKED)
+        sedListenerIdent.consumeSedMottatt(enSedHendelse(), mockk(relaxed = true), ack)
+
+        verify(exactly = 3) { vurderIdentoppdatering.vurderUtenlandskIdent(any()) }
+        verify(exactly = 1) { ack.acknowledge() }
+
+    }
+
+    fun enSedHendelse() = SedHendelse(
+        sektorKode = "P",
+        bucType = BucType.P_BUC_01,
+        sedType = SedType.P2100,
+        rinaSakId = "74389487",
+        rinaDokumentId = "743982",
+        rinaDokumentVersjon = "1",
+        avsenderNavn = "Svensk institusjon",
+        avsenderLand = "SE"
+    ).toJson()
+
+}
+
+@Profile("retryConfigOverride")
+@Component("sedHendelseIdentBehandlerRetryConfig")
+data class SedListenerTestConfig(val initialRetryMillis: Long = 10L)
