c27ad4db6283eea7c69764925c239513d4c5a367
mariampervez <mariam.pervez@nav.no>
2023-06-22 13:37:33 +0200

    B - Velger å ikke oppdatere når vi får beskjed om at kontaktadressen finnes som bostedsadresse
    Co-authored-by: Nicolas.Nordhagen@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/PersonMottakKlient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/PersonMottakKlient.kt
@@ -1,14 +1,13 @@
 package no.nav.eessi.pensjon.pdl
 
 import no.nav.eessi.pensjon.models.PdlEndringOpplysning
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Opplysningstype
 import no.nav.eessi.pensjon.utils.toJson
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
+import org.springframework.http.*
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 import java.util.*
 
@@ -23,16 +22,23 @@ class PersonMottakKlient(private val personMottakRestTemplate: RestTemplate) {
 
         val httpEntity = HttpEntity(endringer.toJson(), createHeaders())
 
-        val response = personMottakRestTemplate.exchange(
-            "/api/v1/endringer",
-            HttpMethod.POST,
-            httpEntity,
-            String::class.java
-        )
-        // Siden vi bruker DefaultResponseErrorHandler vil vi ikke komme hit dersom kallet over returnerer 4xx eller 5xx
-        // - da vil det kastes exception, se DefaultResponseErrorHandler.handleError
-        logger.info("Endringresponse StatusCode: ${response.statusCode}, Body: ${response.body}")
-        return response.statusCode.is2xxSuccessful
+        try {
+            val response = personMottakRestTemplate.exchange(
+                "/api/v1/endringer",
+                HttpMethod.POST,
+                httpEntity,
+                String::class.java
+            )
+            // Siden vi bruker DefaultResponseErrorHandler vil vi ikke komme hit dersom kallet over returnerer 4xx eller 5xx
+            // - da vil det kastes exception, se DefaultResponseErrorHandler.handleError
+            logger.info("Endringresponse StatusCode: ${response.statusCode}, Body: ${response.body}")
+            return response.statusCode.is2xxSuccessful
+        } catch (ex: HttpClientErrorException) {
+            if (ex.statusCode.value() == 409 && Opplysningstype.KONTAKTADRESSE == foersteEndring.opplysningstype ) {
+                return true
+            }
+            throw ex
+        }
     }
 
     private fun createHeaders(): HttpHeaders {
--- src/test/kotlin/no/nav/eessi/pensjon/pdl/PersonMottakKlientTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/pdl/PersonMottakKlientTest.kt
@@ -8,11 +8,12 @@ import no.nav.eessi.pensjon.models.PdlEndringOpplysning
 import no.nav.eessi.pensjon.models.Personopplysninger
 import no.nav.eessi.pensjon.personoppslag.pdl.model.Endringstype
 import no.nav.eessi.pensjon.personoppslag.pdl.model.Opplysningstype
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
@@ -35,7 +36,7 @@ internal class PersonMottakKlientTest {
     fun `opprettPersonopplysning - happy day`() {
         restTemplateCall() returns ResponseEntity("Yo", HttpStatus.ACCEPTED)
 
-        personMottakKlient.opprettPersonopplysning(PdlEndringOpplysning(listOf(dummyPersonOpplysninger)))
+        personMottakKlient.opprettPersonopplysning(PdlEndringOpplysning(listOf(dummyPersonOpplysninger())))
 
         verifyRestTemplateInvocations(1)
     }
@@ -45,9 +46,33 @@ internal class PersonMottakKlientTest {
         restTemplateCall() throws HttpClientErrorException(HttpStatus.BAD_REQUEST)
 
         val ex = assertThrows<HttpClientErrorException> {
-            personMottakKlient.opprettPersonopplysning(PdlEndringOpplysning(listOf(dummyPersonOpplysninger)))
+            personMottakKlient.opprettPersonopplysning(PdlEndringOpplysning(listOf(dummyPersonOpplysninger())))
         }
         assertEquals(HttpStatus.BAD_REQUEST, ex.statusCode)
+
+        verifyRestTemplateInvocations(1)
+    }
+
+    @Test
+    fun `Gitt opprettPersonopplysning - conflict 409 request throws`() {
+        restTemplateCall() throws HttpClientErrorException(HttpStatus.CONFLICT)
+
+        val response = personMottakKlient.opprettPersonopplysning(PdlEndringOpplysning(listOf(dummyPersonOpplysninger(Opplysningstype.KONTAKTADRESSE))))
+
+        assertTrue(response)
+
+        verifyRestTemplateInvocations(1)
+    }
+
+    @ParameterizedTest
+    @EnumSource(Opplysningstype::class, mode = EnumSource.Mode.EXCLUDE, names = ["KONTAKTADRESSE"])
+    fun `Gitt en opprettPersonopplysning med kaster en conflict 409 return false`(opplysningstype:Opplysningstype) {
+        restTemplateCall() throws HttpClientErrorException(HttpStatus.CONFLICT)
+
+        val ex = assertThrows<HttpClientErrorException> {
+            personMottakKlient.opprettPersonopplysning(PdlEndringOpplysning(listOf(dummyPersonOpplysninger(opplysningstype))))
+        }
+        assertEquals(HttpStatus.CONFLICT, ex.statusCode)
 
         verifyRestTemplateInvocations(1)
     }
@@ -72,11 +97,11 @@ internal class PersonMottakKlientTest {
         }
     }
 
-    private val dummyPersonOpplysninger = Personopplysninger(
+    private fun dummyPersonOpplysninger(opplysningstype: Opplysningstype = Opplysningstype.UTENLANDSKIDENTIFIKASJONSNUMMER) = Personopplysninger(
         endringstype = Endringstype.OPPRETT,
         endringsmelding = EndringsmeldingUID(identifikasjonsnummer = "", utstederland = ""),
         ident = "",
-        opplysningstype = Opplysningstype.UTENLANDSKIDENTIFIKASJONSNUMMER
+        opplysningstype = opplysningstype
     )
 
 }
