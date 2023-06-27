cfc0dd3154173ecd2556d1a7dbcec4d59dc1c02d
mariampervez <mariam.pervez@nav.no>
2023-06-27 14:10:02 +0200

    b - legger til mellomrom
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/klienter/SafClient/SafGraphQLModel.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/klienter/SafClient/SafGraphQLModel.kt
@@ -17,7 +17,7 @@ data class SafRequest(
                     "journalpostId " +
                     "datoOpprettet " +
                     "tittel " +
-                    "journalfoerendeEnhet" +
+                    "journalfoerendeEnhet " +
                     "tema " +
                     "behandlingstema " +
                 "}}}",
