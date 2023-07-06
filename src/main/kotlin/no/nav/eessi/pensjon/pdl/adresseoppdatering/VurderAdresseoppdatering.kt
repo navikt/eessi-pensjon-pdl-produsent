095272aad960324abc56cbe8a76d973d3a7acada
Daniel Skarpås <dskarpas@gmail.com>
2023-07-06 12:12:31 +0200

    r - Sletter utkommentert kode
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/VurderAdresseoppdatering.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/VurderAdresseoppdatering.kt
@@ -176,17 +176,4 @@ class VurderAdresseoppdatering(
                 opplysningsId = kontaktadresse.metadata.opplysningsId
             )
         ))
-
-/*    sealed class Result {
-        abstract val description: String
-        abstract val metricTagValueOverride: String?
-
-        val metricTagValue: String
-            get() = metricTagValueOverride ?: description
-    }
-
-    data class Oppdatering(override val description: String, val pdlEndringsOpplysninger: PdlEndringOpplysning, override val metricTagValueOverride: String? = null): Result() {
-        override fun toString() = "Oppdatering(description=$description, pdlEndringsOpplysninger=[omitted], metricTagValueOverride=$metricTagValueOverride)"
-    }
-    data class IngenOppdatering(override val description: String, override val metricTagValueOverride: String? = null): Result()*/
 }
