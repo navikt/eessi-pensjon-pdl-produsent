1e07899dfceebc7e96e5ef13b62f814462e1a316
Daniel Skarpås <dskarpas@gmail.com>
2026-01-02 13:56:47 +0100

    F - Bytter fra println til logger.info

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/AdresseValidering.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/adresseoppdatering/AdresseValidering.kt
@@ -33,7 +33,7 @@ object AdresseValidering {
      * Fra PDL: Norsk bostedsadresse vil alltid ha Freg som master. Utenlandsk bostedsadresse vil alltid ha PDL som master.
      */
     fun erNorskAdresse(meta: Metadata?) : Boolean{
-        return meta?.master.equals("FREG", ignoreCase = true).not().also { logger.warn("erNorskAdresse: " +it) }
+        return meta?.master.equals("FREG", ignoreCase = true).not().also { logger.info("erNorskAdresse: " +it) }
     }
     private fun inneholderPostboksFraser(tekst: String) = postboksFraser.any { tekst.contains(it) }
     private fun inneholderUkjentFraser(tekst: String) = ukjentFraser.any { tekst.contains(it) }
