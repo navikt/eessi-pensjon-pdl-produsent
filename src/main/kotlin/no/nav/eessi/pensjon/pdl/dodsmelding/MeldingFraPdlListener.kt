4d0d6ff728b3911d96413ad7f2d79c301d643f20
Daniel Skarpås <dskarpas@gmail.com>
2026-02-10 13:16:46 +0100

    E - Legger til  io.confluent.kafka.serializers.KafkaAvroDeserializer

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/dodsmelding/MeldingFraPdlListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/dodsmelding/MeldingFraPdlListener.kt
@@ -26,8 +26,8 @@ class MeldingFraPdlListener(
         properties = [
             "auth.exception.retry.interval=30s",
             "auto.offset.reset=earliest",
-            "value.deserializer=io.confluent.kafka.serializers.KafkaAvroDeserializer",
-            "key.deserializer=org.apache.kafka.common.serialization.StringDeserializer",
+            "value.deserializer:io.confluent.kafka.serializers.KafkaAvroDeserializer",
+            "key.deserializer:io.confluent.kafka.serializers.KafkaAvroDeserializer",
             "specific.avro.reader=true",
         ]
     )
