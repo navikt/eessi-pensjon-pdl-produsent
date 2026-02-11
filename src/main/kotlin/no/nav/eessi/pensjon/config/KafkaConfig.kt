9d85652fb5946ecd64684b80e6c544c07a70c6a6
Daniel Skarpås <dskarpas@gmail.com>
2026-02-11 09:55:03 +0100

    E - BOOTSTRAP_SERVERS_CONFIG

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
@@ -92,14 +92,14 @@ class KafkaConfig(
 
 
     private fun consumerConfigsLatestAvro(): Map<String, Any> {
-//        val kafkaBrokers = System.getenv("KAFKA_BROKERS") ?: throw RuntimeException("KAFKA_BROKERS må være satt i miljøet")
+        val kafkaBrokers = System.getenv("KAFKA_BROKERS") ?: throw RuntimeException("KAFKA_BROKERS må være satt i miljøet")
         val schemaRegisty = System.getenv("KAFKA_SCHEMA_REGISTRY") ?: throw RuntimeException("KAFKA_BROKERS må være satt i miljøet")
         val schemaRegistryUser = System.getenv("KAFKA_SCHEMA_REGISTRY_USER") ?: throw RuntimeException("KAFKA_BROKERS må være satt i miljøet")
         val schemaRegistryPassword = System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD") ?: throw RuntimeException("KAFKA_BROKERS må være satt i miljøet")
 //        logger.info("Setter opp consumer med følgende konfigurasjon: bootstrapServers=$kafkaBrokers, schemaRegistry=$schemaRegisty, securityProtocol=$securityProtocol, schemaRegistryUser=$schemaRegistryUser")
         val consumerConfigs =
             mutableMapOf(
-//                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaBrokers,
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaBrokers,
                 "schema.registry.url" to schemaRegisty,
                 "basic.auth.credentials.source" to "USER_INFO",
                 "basic.auth.user.info" to "$schemaRegistryUser:$schemaRegistryPassword",
