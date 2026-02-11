42aaf1f1b0709d602185de5ef22418a43a221f32
Daniel Skarpås <dskarpas@gmail.com>
2026-02-11 12:30:15 +0100

    R - Setter offset til earliest

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
@@ -107,7 +107,7 @@ class KafkaConfig(
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
                 ConsumerConfig.MAX_POLL_RECORDS_CONFIG to "1",
-                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "latest",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
                 ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to "false",
             )
         return consumerConfigs.toMap()
