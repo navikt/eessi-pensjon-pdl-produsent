84b54c4e7deb7b6fd46bc8133ea135ed531be70c
Daniel Skarpås <dskarpas@gmail.com>
2026-04-22 14:33:26 +0200

    R - Acker meldinger
    Co-authored-by: mariam pervez <mariam.pervez@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/pdl/dodsmelding/MeldingFraPdlListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/pdl/dodsmelding/MeldingFraPdlListener.kt
@@ -72,10 +72,10 @@ class MeldingFraPdlListener(
             logger.error("Behandling av hendelse feilet", e)
             throw e
         }
-//        ack.acknowledge()
+        ack.acknowledge()
         messureOpplysningstype.createMetrics()
         messureOpplysningstype.clearAll()
-//        logger.info("Acket personhendelse")
+        logger.info("Acket personhendelse")
     }
 
 
